0bbf60ddac4d037f0df9b13a1018d0a4ae9d9f54
Tiran Chanuka <tiranchanuka1998@gmail.com>
2026-01-30 15:42:25 +0530

    folder structure setup for home page

---
+++ pages/home/HomePage.tsx
@@ -0,0 +1,16 @@
+"use client";
+import HeroSection from "./HeroSection";
+import ServicesSection from "./ServicesSection";
+import BranchesSection from "./BranchesSection";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <ServicesSection />
+      <BranchesSection />
+    </>
+  );
+};
+
+export default HomePage;
