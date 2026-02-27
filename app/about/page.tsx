769683bc3db46e44127abe2faf572f0b22b70438
miriyamtrishni <114347445+miriyamtrishni@users.noreply.github.com>
2026-02-27 12:39:13 +0530

    remove unused dark mode context and simplify AboutPage component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 import About from '@/components/About'
 import DarkModeToggle from '@/components/DarkModeToggle'
-import { useDarkMode } from '@/context/DarkModeContext'
 
 export default function AboutPage() {
-  const { isDarkMode } = useDarkMode()
-
   return (
-    <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
+    <div>
       <main>
         <About />
       </main>
